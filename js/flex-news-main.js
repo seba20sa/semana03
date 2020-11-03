ae3b35321184b87f245cb43dfb8b7bac209c21f4
Sebastian Sileoni <seba20sa@gmail.com>
2020-11-03 14:37:30 -0300

    made modifications to the checkFirstPassword funcion

--- js/flex-news-main.js
+++ js/flex-news-main.js
@@ -1,15 +1,9 @@
 //Variables
 var burgerButton = document.getElementById('burger-menu');
 var hiddenNavBar = document.getElementById('hidden-nav-bar');
-
-
-
 //Event listeners
 burgerButton.addEventListener('click', OpenMenuBar);
-
-
 //Actions
-
 function OpenMenuBar(e){
     hiddenNavBar.style.display = 'block';
     burgerButton.style.display= 'none';
